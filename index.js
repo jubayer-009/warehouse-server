a916558a4a19b9c62e35f0ca3f45536ff7fb72d8
unknown <013jubayer@gmail.com>
2022-11-01 23:42:13 +0600

    basic setup

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const cors = require("cors");
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.xsnumx1.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run(){
+  try{
+        await client.connect();
+
+  }
+  finally{
+
+  }
+console.log("db Connected");
+
+}
+run().catch(console.dir);
+
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
